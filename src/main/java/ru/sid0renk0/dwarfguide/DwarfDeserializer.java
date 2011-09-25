5b6689a744270c9c1d255d4592a8f1431fbea109
Dmitry Sidorenko <rustydragon@gmail.com>
2011-09-25 22:10:29 +0400

    Added Javolution for XML and extra fun. First version on github

---
+++ src/main/java/ru/sid0renk0/dwarfguide/DwarfDeserializer.java
@@ -0,0 +1,13 @@
+package ru.sid0renk0.dwarfguide;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Dmitry Sidorenko
+ */
+public class DwarfDeserializer {
+    @SuppressWarnings({"unused"})
+    private static final Logger LOGGER = LoggerFactory.getLogger(DwarfDeserializer.class);
+
+}
