5c192c3747b9ed2910e1ddc7b4983f5cb93fd58d
Dmitry Sidorenko <rustydragon@gmail.com>
2011-09-26 15:03:45 +0400

    Updated copyright

--- src/test/java/ru/sid0renk0/dwarfguide/model/ExperienceFactoryTest.java
+++ src/test/java/ru/sid0renk0/dwarfguide/model/ExperienceFactoryTest.java
@@ -1,18 +1,33 @@
+/*
+ * Copyright (c) 2010-2011, Dmitry Sidorenko. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ru.sid0renk0.dwarfguide.model;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static ru.sid0renk0.dwarfguide.model.ExperienceFactory.getSkillLevel;
 
 /**
  * @author Dmitry Sidorenko
  */
-public class ExperienceFactoryTest{
+public class ExperienceFactoryTest {
     @SuppressWarnings({"unused"})
     private static final Logger LOGGER = LoggerFactory.getLogger(ExperienceFactoryTest.class);
 
