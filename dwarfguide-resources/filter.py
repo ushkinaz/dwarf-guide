ec4d6921646e85df678cad21461008124a1cb066
Dmitry Sidorenko <rustydragon@gmail.com>
2011-10-08 21:42:14 +0400

    Upgraded script

--- dwarfguide-resources/filter.py
+++ dwarfguide-resources/filter.py
@@ -1,15 +1,28 @@
-import re
 import os
-from os.path import join, getsize
+import re
+from os.path import join
+
+from shutil import copy
 from subprocess import call
 
 __author__ = 'Dmitry Sidorenko'
 
+STRUCTURE = "structure.mps"
+
 for root, dirs, files in os.walk('../dwarfguide-mps/'):
     for name in files:
-        if name == "structure.mps":
-            for line in open(join(root, name)):
-                m = re.search('<property name="iconPath" nameId=".*" value="\$\{DWARVEN_PATH\}\/(.*)"', line)
+        if name == STRUCTURE:
+            copy(join(root, name), join(root, name + ".bak"))
+            output = open(join(root, name), "w")
+            for line in open(join(root, name + ".bak")):
+                m = re.search('<property name="iconPath" nameId=".*" value=".*\/dwarfguide-resources\/local\/(.*)"',
+                              line)
                 if m:
-                    print (m.group(1))
-                    call(["git", "add", "../" + m.group(1)])
+                    icon_name = m.group(1)
+                    print(icon_name)
+                    copy("./local/" + icon_name, "./icons/" + icon_name)
+                    call(["git", "add", "./icons/" + icon_name])
+                    line = line.replace("/dwarfguide-resources/local/" + icon_name,
+                                        "/dwarfguide-resources/icons/" + icon_name)
+                output.write(line)
+            output.close()
